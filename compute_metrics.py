7d827bd584734dfb3f1c4bba8209d149ed0c371e
Minhee Lee <minigimbob@gmail.com>
2024-02-23 08:50:22 +0900

    ss

--- compute_metrics.py
+++ compute_metrics.py
@@ -3,67 +3,86 @@ from collections import defaultdict
 
 from scipy.stats import kendalltau
 from statistics import mean, stdev
+from sklearn.metrics import mean_squared_error, balanced_accuracy_score
 
 from utils import *
 
 
-def compute_tauc(bin_file_name):
-    data = load_binary(bin_file_name)
-    pred_list, true_list = [], []
-    for _, data_pr in data.items():
-        pred_list.append(int(data_pr['pred']))
-        true_list.append(int(data_pr['true']))
-    tau_c_value = kendalltau(x=true_list, y=pred_list).statistic
-    return tau_c_value
+MODEL_TYPE_DICT = {
+    # single task
+    'audio_midi_cqt5_ps_v5' : 'CQT',
+    'audio_midi_pianoroll_ps_5_v4' : 'PR',
+    'audio_midi_multi_ps_v5' : "MM",
+
+    # multi task with era
+    'audio_midi_cqt5era_v1' : 'CQT',
+    'audio_midi_pr5era_v1' : 'PR',
+
+    # multi task with multi ranking
+    'audio_midi_cqt5multiranking_v10' : 'CQT',
+    'audio_midi_pr5multiranking_v10' : 'PR'
+}
 
 
-def mean_std(data):
-    mean_val = mean(data)
-    std_val = stdev(data)
+def get_mse_macro(y_true, y_pred):
+    mse_each_class = []
+    for true_class in set(y_true):
+        tt, pp = zip(*[[tt, pp] for tt, pp in zip(y_true, y_pred) if tt == true_class])
+        mse_each_class.append(mean_squared_error(y_true=tt, y_pred=pp))
+    return mean(mse_each_class)
 
-    return {"data": data, "mean": mean_val, "std": std_val}
+
+def compute_metrics(dir_path, model_type):
+    mse, acc, tau_c = [], [], []
+    for split in range(5):
+        bin_file_name = f'{dir_path}/{model_type}_split_{split}.bin'
+        data = load_binary(bin_file_name)
+
+        pred_list, true_list = [], []
+        for data_pr in data.values():
+            true_list.append(int(data_pr['true']))
+            pred_list.append(int(data_pr['pred']))
+        
+        mse.append(get_mse_macro(true_list, pred_list))
+        acc.append(balanced_accuracy_score(true_list, pred_list))
+        tau_c.append(kendalltau(x=true_list, y=pred_list).statistic)
+
+    metrics = {
+        "mse": mse,
+        "acc": acc,
+        "tau_c": tau_c
+    }
+
+    return metrics
+
+
+# def mean_std(data):
+#     mean_val = mean(data)
+#     std_val = stdev(data)
+
+#     return {"data": data, "mean": mean_val, "std": std_val}
 
 
 def print_for_paper(result):
-    model_type_dict = {
-        # single task
-        'audio_midi_cqt5_ps_v5' : 'CQT',
-        'audio_midi_pianoroll_ps_5_v4' : 'PR',
-        'audio_midi_multi_ps_v5' : "MM",
-
-        # multi task with era
-        'audio_midi_cqt5era_v1' : 'CQT',
-        'audio_midi_pr5era_v1' : 'PR',
-
-        # multi task with multi ranking
-        'audio_midi_cqt5multiranking_v10' : 'CQT',
-        'audio_midi_pr5multiranking_v10' : 'PR'
-    }
-
-    model_types = list(model_type_dict.keys())
+    model_types = list(MODEL_TYPE_DICT.keys())
     assert len(model_types) == len(result), f"{len(model_types)} != {len(result)}"
     assert set(model_types) == set(result.keys()), f"{model_types} != {set(result.keys())}"
 
     for model_type in model_types:
         data = result[model_type]
-        formatted_mean = f"{data['mean']:.3f}".lstrip('0')
-        formatted_std = f"{data['std']:.3f}".lstrip('0')
+        formatted_mean = f"{mean(data):.3f}".lstrip('0')
+        formatted_std = f"{stdev(data):.3f}".lstrip('0')
 
-        print(f"& {model_type_dict[model_type]}$_{{5}}$ & {formatted_mean} ({formatted_std}) \\\\")
+        print(f"& {MODEL_TYPE_DICT[model_type]}$_{{5}}$ & {formatted_mean} ({formatted_std}) \\\\")
 
 
 if __name__ == "__main__":
     inference_type = "multi"
+    dir_path = f"{inference_type}/logits"
     result = defaultdict(list)
-    for filename in os.listdir(f"{inference_type}/logits"):
-        if not filename.endswith(".bin"):
-            continue
 
-        model_type = filename.split("_split")[0]
-        result[model_type].append(compute_tauc(f"{inference_type}/logits/{filename}"))
+    for model_type in MODEL_TYPE_DICT.keys():
+        result[model_type].append(compute_metrics(dir_path, model_type))
 
-    for model_type, data in result.items():
-        result[model_type] = mean_std(data)
-
-    save_json(result, f"{inference_type}_tauc.json")
-    print_for_paper(result)
+    save_json(result, f"{inference_type}_metrics.json")
+    # print_for_paper(result)
