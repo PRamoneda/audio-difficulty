01d53f4f9d5054d2e56547ff1e356bc39c9e9d44
Minhee Lee <minigimbob@gmail.com>
2024-02-23 07:40:45 +0900

    done computing tauc

--- compute_tauc.py
+++ compute_tauc.py
@@ -1,20 +1,45 @@
-from statistics import mean, stdev
+import os
+from collections import defaultdict
+
 from scipy.stats import kendalltau
+from statistics import mean, stdev
 
 from utils import *
 
 
 def compute_tauc(bin_file_name):
     data = load_binary(bin_file_name)
-    tau_c = []
     pred_list, true_list = [], []
-    for key, data_pr in data.items():
+    for _, data_pr in data.items():
         pred_list.append(int(data_pr['pred']))
         true_list.append(int(data_pr['true']))
     tau_c_value = kendalltau(x=true_list, y=pred_list).statistic
-    print(f"{tau_c_value:.3f}")
+    return tau_c_value
+
+
+def mean_std(data):
+    mean_val = mean(data)
+    std_val = stdev(data)
+
+    return {"data": data, "mean": mean_val, "std": std_val}
+
+
+def print_for_paper(result):
+    for model_type, data in result.items():
+        print(f"{model_type} & {data['mean']:.4f} & {data['std']:.4f} \\\\")
 
 
 if __name__ == "__main__":
-    for split in range(5):
-        compute_tauc(f"multi/logits/audio_midi_cqt5_ps_v5_split_{split}.bin")
+    result = defaultdict(list)
+    for filename in os.listdir("multi/logits"):
+        if not filename.endswith(".bin"):
+            continue
+
+        model_type = filename.split("_split")[0]
+        result[model_type].append(compute_tauc(f"multi/logits/{filename}"))
+
+    for model_type, data in result.items():
+        result[model_type] = mean_std(data)
+
+    save_json(result, "multi_tauc.json")
+    print_for_paper(result)
