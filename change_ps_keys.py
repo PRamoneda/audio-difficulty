95565d8ff0b720f43524386ba4e754ca1458ab22
Minhee Lee <minigimbob@gmail.com>
2024-02-23 19:31:43 +0900

    ss

--- change_ps_keys.py
+++ change_ps_keys.py
@@ -1,13 +1,18 @@
 import os
+from pathlib import Path
 from utils import *
+from collections import OrderedDict
 
 if __name__ == "__main__":
-    for file_name in os.listdir('benchmark_multiperformances'):
+    dir_name = Path('benchmark_multiperformances')
+    for file_name in os.listdir(dir_name):
         if not file_name.endswith('.json'):
             continue
-        data = load_json(f'benchmark_multiperformances/{file_name}')
+        
+        json_filename = dir_name / file_name
+        data = load_json(f'{dir_name}/{file_name}')
         # Assuming the dictionary has at least one key-value pair
         first_key = list(data.keys())[0]
         new_key = first_key + '_from_ps'
-        data[new_key] = data.pop(first_key)
-        save_json(data, f'benchmark_multiperformances/{file_name}')
+        data_new = OrderedDict([(new_key, data.pop(first_key))] + list(data.items()))
+        save_json(data_new, json_filename)
